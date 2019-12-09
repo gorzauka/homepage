dfc3b4e558dbcbe2a8c58a30f2144bc57a25fee9
Gosia Siłkowska <silkowska.m@gmail.com>
2019-12-09 18:17:32 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+const name= "Gosia";
+const age= 31;
+
+console.log(name);
+console.log(age);
+
+console.log(`My name is ${name} and I am ${age} years old`);
+
+const coder= document.querySelector(".work__heading--js");
+
+coder.innerHTML = "My code";
