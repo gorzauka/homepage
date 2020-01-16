76898470abf252b44da897e250e569dabaa0c2e9
Gosia Siłkowska <silkowska.m@gmail.com>
2020-01-16 19:44:35 +0100

    feat: hamburger menu

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ console.log(age);
 
 console.log(`My name is ${name} and I am ${age} years old`);
 
-const coder= document.querySelector(".work__heading--js");
-
-coder.innerHTML = "My code";
-
 function hello(name, age){
 return `Goodbye, ${name},${age}!`
 };
 
 console.log(hello("Maciek",34));
+
+const navigationSwitcher = document.querySelector(".navigation__switcher--js");
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
